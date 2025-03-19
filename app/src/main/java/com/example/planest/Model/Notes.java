0d22032a14c4d7683acd18b90c5c61b0a0899866
ramthetiger <ramyanindita@gmail.com>
2025-03-19 17:40:31 +0700

    Create Function on to firebase,

--- app/src/main/java/com/example/planest/Model/Notes.java
+++ app/src/main/java/com/example/planest/Model/Notes.java
@@ -1,4 +1,58 @@
 package com.example.planest.Model;
 
 public class Notes {
+
+    private String notes_id;
+    private String Title;
+    private String notes;
+    private String last_modified;
+    private String password;
+
+    public Notes(String title, String notes, String last_modified, String password) {
+
+        Title = title;
+        this.notes = notes;
+        this.last_modified = last_modified;
+        this.password = password;
+    }
+
+    public String getNotes_id() {
+        return notes_id;
+    }
+
+    public void setNotes_id(String notes_id) {
+        this.notes_id = notes_id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getLast_modified() {
+        return last_modified;
+    }
+
+    public void setLast_modified(String last_modified) {
+        this.last_modified = last_modified;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- app/src/main/java/com/example/planest/NotesActivity.java
+++ app/src/main/java/com/example/planest/NotesActivity.java
@@ -2,18 +2,26 @@ package com.example.planest;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.planest.Model.Notes;
 import com.example.planest.databinding.ActivityNotesBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NotesActivity extends AppCompatActivity {
 
     private ActivityNotesBinding binding;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,22 @@ public class NotesActivity extends AppCompatActivity {
         initUI();
     }
     private void initUI(){
-
+        binding.btnSync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notes notes = new Notes(binding.etHeader.getText().toString(), binding.etContent.getText().toString(), "test", "");
+                database.getReference().child("notes").push().setValue(notes).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(NotesActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(NotesActivity.this, "Failed to Sync", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
